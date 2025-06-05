7729140d96a7d7b45c2096c965ec3fd5fcb6b618
GurMeow <117115522+GurMeow@users.noreply.github.com>
2025-06-05 21:28:00 +0300

    Update main.py
    added new values and requests

--- Chess_Flask_Fixed/main.py
+++ Chess_Flask_Fixed/main.py
@@ -376,6 +376,9 @@ app.static_folder = static_dir
 
 bot = False
 
+player_time = 600
+player_timer = False
+
 @app.route("/")
 def load_home_page():
     global bot
@@ -404,6 +407,30 @@ def change_bot():
 def get_bot():
     global bot
     return jsonify(bot)
+
+
+@app.route("/change_time", methods=["POST"])
+def change_time():
+    global player_time
+    print("e")
+    player_time = int(request.form.get("player_time"))
+    print(player_time)
+    return jsonify(player_time)
+
+
+@app.route("/change_timer", methods=["POST"])
+def change_timer():
+    global player_timer
+    player_timer = request.form.get("player_timer")
+    player_timer = player_timer.lower() == "true"
+    print(player_timer)
+    return jsonify(player_timer)
+
+
+@app.route("/get_time")
+def get_time():
+    global player_time, player_timer
+    return jsonify(player_time, player_timer)
 
 
 @app.route("/get_move", methods=['POST'])
