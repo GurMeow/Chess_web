09dcbbb504e4de7385d94029ad7ba8d4eae7d581
etidhar <105047514+etidhar@users.noreply.github.com>
2025-06-06 15:22:39 +0300

    Add files via upload

---
+++ Chess_Flask_Fixed/game_state.py
@@ -0,0 +1,15 @@
+white_in_check = False
+any_color_in_check = False
+black_in_check = False
+moves_calculated = 0
+
+
+def init():
+    global white_in_check, any_color_in_check, black_in_check, moves_calculated
+    white_in_check = False
+    any_color_in_check = False
+    black_in_check = False
+    moves_calculated = 0
+
+
+init()
